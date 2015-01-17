b01a98de741221e093dfad8a204d16e5b8f1e0c4
KRRySS <jedynakpoczta@gmail.com>
2015-01-17 14:44:15 +0100

    Dodanie zmian w konfiguracji na mvc.

--- api/src/main/java/pl/edu/wat/swp/Application.java
+++ api/src/main/java/pl/edu/wat/swp/Application.java
@@ -1,19 +1,41 @@
 package pl.edu.wat.swp;
 
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableWebMvc
 public class Application
 {
 
+    private static Logger logger = Logger.getLogger( Application.class );
+    
     public static void main( String[] args )
     {
-        SpringApplication.run( Application.class, args );
+        ApplicationContext ctx = SpringApplication.run( Application.class, args );
+
+        // sprawdzam dostępną listę beanów w Spring Boot dla tej konfiguracji
+        if ( logger.isDebugEnabled() )
+        {
+            logger.debug( "Let's inspect the beans provided by Spring Boot" );
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort( beanNames );
+
+            for ( String beanName : beanNames )
+            {
+                logger.debug( beanName );
+            }
+        }
     }
 
 }
